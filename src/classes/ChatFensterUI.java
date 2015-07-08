ba8159f3626a445b3f70a496777d3055b5d89b41
Inga <inga.schwarze2010@gmail.com>
2015-07-08 19:07:38 +0200

    ip eingegeben

--- src/classes/ChatFensterUI.java
+++ src/classes/ChatFensterUI.java
@@ -63,7 +63,7 @@ public class ChatFensterUI extends Application {
 
 	private void setUpNetworking() {
 		try {
-			Socket socket = new Socket("127.0.0.1", 8080);
+			Socket socket = new Socket("89.247.113.62", 8080);
 			InputStream in = socket.getInputStream();
 			OutputStream out = socket.getOutputStream();
 			InputStreamReader streamReader = new InputStreamReader(in);
