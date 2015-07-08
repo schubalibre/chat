234ebd52828636244278d2c11370b052ba825c53
Inga <inga.schwarze2010@gmail.com>
2015-07-08 13:55:17 +0200

    Meine Version

---
+++ src/classes/VerySimpleChatServer.java
@@ -0,0 +1,34 @@
+package classes;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class VerySimpleChatServer {
+	
+	
+	public static void main(String[] args) {
+		new VerySimpleChatServer().start();
+	}
+
+	private void start() {
+		ArrayList<PrintWriter> clientOutputStream = new ArrayList<PrintWriter>();
+		
+		try {
+			ServerSocket serverSocket = new ServerSocket(8080);
+			
+			while(true) {
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				clientOutputStream.add(writer);
+				
+				Thread t = new Thread(new ClientHandler(clientSocket, clientOutputStream));
+				t.start();
+				System.out.println("client connection established");
+			}
+		} catch (Exception ex) { ex.printStackTrace(); }
+		
+	}
+
+}
